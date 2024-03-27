50880308088ad6812b4bd8f19e3baad766d9b441
Ho Tan Anh <anh152k3@gmail.com>
2024-03-27 23:00:41 +0700

    edit userService

--- src/main/java/com/tananh/service/userService.java
+++ src/main/java/com/tananh/service/userService.java
@@ -1,5 +1,9 @@
 package com.tananh.service;
 
+import com.tananh.exception.UserException;
+import com.tananh.modal.User;
+
 public interface userService {
-	
+	public User findUserById(Long id)throws UserException;
+	public User findUserByJWT(String jwt)throws UserException ;
 }
