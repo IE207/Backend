d3a5889e95eed2596e663a7714ad0a3c92ed2d31
Ho Tan Anh <anh152k3@gmail.com>
2024-04-04 16:53:09 +0700

    edit postServiceImpl

--- src/main/java/com/tananh/service/PostServiceImplement.java
+++ src/main/java/com/tananh/service/PostServiceImplement.java
@@ -2,22 +2,46 @@ package com.tananh.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tananh.dto.UserDto;
 import com.tananh.exception.PostException;
 import com.tananh.exception.UserException;
 import com.tananh.modal.Post;
+import com.tananh.modal.User;
+import com.tananh.responsitory.PostResponsitory;
 
 public class PostServiceImplement implements PostService{
 
+	@Autowired userService userService;
+	@Autowired PostResponsitory postResponsitory;
+	
 	@Override
 	public Post createPost(Post post, Integer userId) throws UserException {
-		// TODO Auto-generated method stub
-		return null;
+		User user = userService.findUserById(userId);
+
+        UserDto userDto = new UserDto();
+        userDto.setEmail(user.getEmail());
+        userDto.setUserId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setImageURL(user.getImageURL());
+        userDto.setUserName(user.getUserName());
+        post.setUser(userDto);
+        Post savedPost = postResponsitory.save(post);
+		return savedPost;
 	}
 
 	@Override
 	public Post updatePost(Post postUpdate, Integer postId) throws PostException {
-		// TODO Auto-generated method stub
-		return null;
+			if(postUpdate==null) {
+				throw new PostException("Thông tin cập nhật NULL");
+			}
+			Post post = findPostById(postId);
+			post.setCaption(postUpdate.getCaption());
+			post.setCreateAt(postUpdate.getCreateAt());
+			post.setImage(postUpdate.getImage());
+			
+			return postResponsitory.save(post);
 	}
 
 	@Override
